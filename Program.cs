32166be99b6bdbe423863496bdc175f97f0fc2f3
nryzhenkov <nryzhenkov@yandex.ru>
2019-02-07 00:15:23 +0300

    Add overload CreateArray

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ namespace MyArray
                 return new int[size];
             return null;
         }
+
+        static public void CreateArray(int size, out int[] array) {
+            if (size > 0)
+                array = new int[size];
+            else
+                array = null;
+        }
     }
 }
 
@@ -25,7 +32,11 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             int[] a = MyArray.FirstClass.CreateArray(4);
+            MyArray.FirstClass.CreateArray(5, out int[] b);
             foreach (int i in a)
+                Console.WriteLine(i);
+            Console.WriteLine("next b");
+            foreach (int i in b)
                 Console.WriteLine(i);
             Console.Read();
         }
