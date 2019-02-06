c67773c96554ae04ddc546a4a3f5d7c88731080c
nryzhenkov <nryzhenkov@yandex.ru>
2019-02-06 22:57:25 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArray
+{
+    static class FirstClass {
+        static public int[] CreateArray(int size) {
+            if (size > 0)
+                return new int[size];
+            return null;
+        }
+    }
+}
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] a = MyArray.FirstClass.CreateArray(4);
+            foreach (int i in a)
+                Console.WriteLine(i);
+            Console.Read();
+        }
+    }
+}
