b70268e3de65097f4b8bf45cbba51c3b555ebbdc
nryzhenkov <nryzhenkov@yandex.ru>
2019-02-08 21:01:28 +0300

    Add SecondClass

--- FirstClass.cs
+++ FirstClass.cs
@@ -3,6 +3,7 @@ namespace MyArray
 {
     static public class FirstClass
     {
+        
         static public int[] CreateArray(int size)
         {
             if (size > 0)
@@ -16,6 +17,45 @@ namespace MyArray
                 array = new int[size];
             else
                 array = null;
+        }
+    }
+
+    public class SecondClass
+    {
+        int[] localArray;
+
+        bool isNull;
+
+        public SecondClass(int size)
+        {
+            if (size > 0)
+            {
+                localArray = new int[size];
+                isNull = false;
+            }
+            else
+            {
+                localArray = null;
+                isNull = true;
+            }
+
+        }
+
+        public SecondClass()
+        {
+            localArray = null;
+            isNull = true;
+        }
+
+        public bool CreateArray(int size)
+        {
+            if (isNull)
+            {
+                localArray = new int[size];
+                return true;
+            }
+
+            return false;
         }
     }
 }
